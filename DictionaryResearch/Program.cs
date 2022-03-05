0c77bd46e9a6723f48a6fae39bbde416b8d310e1
Евгений Куликов <ekueku2001@icloud.com>
2022-03-05 11:15:50 +0500

    Initial commit

---
+++ DictionaryResearch/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DictionaryResearch;
+
+internal static class Program
+{
+	internal static void Main()
+	{
+		// Активность «Исследование Dictionary»
+		// 
+		var firstArray = new[] { 1, 3, 2, 4, 2, 4 };
+		var secondArray = new[] { 1, 6, 7, 8, 9, 4 };
+
+		var hashSet = new HashSet<int>();
+
+		foreach (var i in firstArray)
+		{
+			hashSet.Add(i);
+		}
+
+		foreach (var i in secondArray)
+		{
+			hashSet.Add(i);
+		}
+
+		foreach (var i in Enumerable.Range(10,1000000000))
+		{
+			hashSet.Add(i * 101);
+		}
+		
+		Console.WriteLine(hashSet.Contains(2));
+		Console.WriteLine(hashSet.Contains(5));
+	}
+}
