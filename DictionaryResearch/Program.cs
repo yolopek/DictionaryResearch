d2af4653ea759165f1fcf82983ceab8c1d14e7f3
Евгений Куликов <ekueku2001@icloud.com>
2022-03-05 12:16:33 +0500

    Выделил DictionaryLength

--- DictionaryResearch/Program.cs
+++ DictionaryResearch/Program.cs
@@ -4,19 +4,21 @@ namespace DictionaryResearch;
 
 internal static class Program
 {
+	internal const int DictionaryLength = 500000;
+
 	internal static void Main()
 	{
 		Helper.CheckBuckets(); // для 50к: 75431
 
 		var defaultFilling = new Action(() =>
 		{
-			var count = Enumerable.Range(0, 500000).ToDictionary(i => i, i => "Value").Count;
+			var count = Enumerable.Range(0, DictionaryLength).ToDictionary(i => i, i => "Value").Count;
 		});
-		
+
 		// TODO Сделать Action, меделнно заполняющий Dictionary;
-		
+
 		Console.WriteLine(Helper.MeasureTime(defaultFilling));
-		
+
 		// Какое время на вашем ПК для обоих Action
 	}
 }
@@ -25,7 +27,7 @@ internal static class Helper
 {
 	internal static void CheckBuckets()
 	{
-		var dictionary = Enumerable.Range(0, 500000).ToDictionary(i => i, i => "Value");
+		var dictionary = Enumerable.Range(0, Program.DictionaryLength).ToDictionary(i => i, i => "Value");
 		var count = dictionary.Count;
 		Console.WriteLine(); // debug button
 	}
