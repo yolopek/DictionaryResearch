2ee1a4ec8fac57f90d307a4c954842948da984d7
Евгений Куликов <ekueku2001@icloud.com>
2022-03-05 11:54:51 +0500

    Составил задачу

--- DictionaryResearch/Program.cs
+++ DictionaryResearch/Program.cs
@@ -1,32 +1,43 @@
-﻿namespace DictionaryResearch;
+﻿using System.Diagnostics;
+
+namespace DictionaryResearch;
 
 internal static class Program
 {
 	internal static void Main()
 	{
-		// Активность «Исследование Dictionary»
-		// 
-		var firstArray = new[] { 1, 3, 2, 4, 2, 4 };
-		var secondArray = new[] { 1, 6, 7, 8, 9, 4 };
+		Helper.CheckBuckets(); // для 50к: 75431
 
-		var hashSet = new HashSet<int>();
-
-		foreach (var i in firstArray)
+		var defaultFilling = new Action(() =>
 		{
-			hashSet.Add(i);
-		}
-
-		foreach (var i in secondArray)
-		{
-			hashSet.Add(i);
-		}
-
-		foreach (var i in Enumerable.Range(10,1000000000))
-		{
-			hashSet.Add(i * 101);
-		}
+			var count = Enumerable.Range(0, 500000).ToDictionary(i => i, i => "Value").Count;
+		});
 		
-		Console.WriteLine(hashSet.Contains(2));
-		Console.WriteLine(hashSet.Contains(5));
+		// TODO Сделать Action, меделнно заполняющий Dictionary;
+		
+		Console.WriteLine(Helper.MeasureTime(defaultFilling));
+		
+		// Какое время на вашем ПК для обоих Action
+	}
+}
+
+internal static class Helper
+{
+	internal static void CheckBuckets()
+	{
+		var dictionary = Enumerable.Range(0, 500000).ToDictionary(i => i, i => "Value");
+		var count = dictionary.Count;
+		Console.WriteLine(); // debug button
+	}
+
+	internal static long MeasureTime(Action action)
+	{
+		var time = new Stopwatch();
+		GC.Collect();
+		GC.WaitForPendingFinalizers();
+		time.Restart();
+		action.Invoke();
+		time.Stop();
+		return time.ElapsedMilliseconds;
 	}
 }
